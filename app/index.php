55bc0200e78e02935706594674c521d284ff0de5
alef a <alef.2212@gmail.com>
2019-01-31 22:44:14 -0200

    start

---
+++ app/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta chartset="utf8" />
+        <title>Exemplo 01 - PHP</title>
+    </head>
+
+    <body>
+        <h1>Exibição de Dados com php</h1>
+        <p> <?php print 'Este texto foi escrito em PHP' ?></p>
+    </body>
+
+    </html>
